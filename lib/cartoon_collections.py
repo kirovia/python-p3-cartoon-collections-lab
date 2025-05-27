1955d320b3e18ff652a2c086583f51be80f6aa2b
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-05-27 18:49:53 -0400

    Passes all tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf) + 1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [call.capitalize() + '!' for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+    for cheese in cheeses:
+        if cheese in foods:
+            return cheese
+    return None
